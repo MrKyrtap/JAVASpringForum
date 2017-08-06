1574c66805fc392e6dc21e00e40980520b3f3071
PatrykGrudnik <pacy04@gmail.com>
2017-08-06 15:48:56 +0200

    make it better

--- src/main/java/com/grudnik/config/SecurityConfig.java
+++ src/main/java/com/grudnik/config/SecurityConfig.java
@@ -56,13 +56,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
                 .authenticated().and().csrf().disable().formLogin()
                 .loginPage("/login").failureUrl("/login?error=true")
-                .defaultSuccessUrl("/profile")
+                .defaultSuccessUrl("/")
                 .usernameParameter("mail")
                 .passwordParameter("password")
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login").and().exceptionHandling();
-              //  .accessDeniedPage("/access-denied");
     }
 
 }
